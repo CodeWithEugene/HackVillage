dc726841852cb33156daf7993a9eabbb6a0cb0eb
Eugene Mutembei <103780583+CodeWithEugene@users.noreply.github.com>
2026-09-25 10:02:49 +0300

    feat(ui): link the footer's Salamander logo to salamandertechhub.com
    Opens in a new tab.

--- components/patterns/site-footer.tsx
+++ components/patterns/site-footer.tsx
@@ -118,12 +118,19 @@ export function SiteFooter() {
             </li>
           </ul>
           <p className="mt-6 text-sm text-body-copy">In partnership with:</p>
-          {/* eslint-disable-next-line @next/next/no-img-element -- SVG wordmark, no raster source */}
-          <img
-            src="/images/salamander-logo-yellow.svg"
-            alt="Salamander Tech Hub"
-            className="mt-3 h-9 w-auto"
-          />
+          <a
+            href="https://salamandertechhub.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-3 inline-block transition-opacity hover:opacity-80"
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element -- SVG wordmark, no raster source */}
+            <img
+              src="/images/salamander-logo-yellow.svg"
+              alt="Salamander Tech Hub"
+              className="h-9 w-auto"
+            />
+          </a>
         </div>
       </div>
 
